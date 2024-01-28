9f63b0f77389b1b954e10f6f8067214e88f7f106
vanilla <vanillaiice@tutanota.com>
2024-01-28 08:25:13 +0300

    Initial commit

---
+++ cmd/itpg-backend/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"itpg"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:    "Is This Professor Good - Backend",
+		Suggest: true,
+		Version: "v0.0.1",
+		Authors: []*cli.Author{{Name: "Vanillaiice", Email: "vanillaiice@proton.me"}},
+		Usage:   "handle http requests and database transactions",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "port",
+				Aliases: []string{"p"},
+				Value:   "5555",
+			},
+			&cli.PathFlag{
+				Name:     "dbname",
+				Aliases:  []string{"db"},
+				Required: true,
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			return itpg.Run(ctx.String("port"), ctx.Path("dbname"))
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
