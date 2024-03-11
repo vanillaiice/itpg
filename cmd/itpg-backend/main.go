a1eae140c240fa39da51d1ef3da6fc8433b4fa20
vanilla <vanillaiice@tutanota.com>
2024-03-11 22:51:16 +0300

    chore: bump to v0.1.5

--- cmd/itpg-backend/main.go
+++ cmd/itpg-backend/main.go
@@ -12,7 +12,7 @@ func main() {
 	app := &cli.App{
 		Name:    "itpg-backend",
 		Suggest: true,
-		Version: "v0.1.4",
+		Version: "v0.1.5",
 		Authors: []*cli.Author{{Name: "vanillaiice", Email: "vanillaiice1@proton.me"}},
 		Usage:   "Backend server for ITPG, handles database transactions and user state management through HTTP(S) requests.",
 		Flags: []cli.Flag{
