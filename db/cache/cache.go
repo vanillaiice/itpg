9cd781011c09c2dd000317786d6a314580ca0951
vanilla <vanillaiice@tutanota.com>
2024-06-06 19:13:31 +0300

    chore: add code comments

--- db/cache/cache.go
+++ db/cache/cache.go
@@ -7,13 +7,16 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+// Cache is a cache implementation.
 type Cache struct {
 	client *redis.Client
 	ctx    context.Context
 }
 
+// ErrRedisNil is returned when a key is not found in redis.
 const ErrRedisNil = redis.Nil
 
+// New initializes a new cache.
 func New(url string, ctx context.Context) (*Cache, error) {
 	opts, err := redis.ParseURL(url)
 	if err != nil {
@@ -32,14 +35,17 @@ func New(url string, ctx context.Context) (*Cache, error) {
 	}, nil
 }
 
+// Close closes the cache.
 func (c *Cache) Close() error {
 	return c.client.Close()
 }
 
+// Set sets a value in the cache.
 func (c *Cache) Set(key string, value any, ttl time.Duration) error {
 	return c.client.Set(c.ctx, key, value, ttl).Err()
 }
 
+// Get gets a value from the cache.
 func (c *Cache) Get(key string) (string, error) {
 	return c.client.Get(c.ctx, key).Result()
 }
